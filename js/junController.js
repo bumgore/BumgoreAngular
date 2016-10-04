6a3d0a2dd47424bc1280bdf7ee1d1133f5ed9caf
bumgore <junalfiechua@gmail.com>
2016-10-05 06:59:50 +0800

    First Commit

---
+++ js/junController.js
@@ -0,0 +1,41 @@
+var junApp = angular.module('junApp', ['ngRoute']);
+
+junApp.config(function ($routeProvider) {
+
+    $routeProvider
+    .when('/', {
+        controller: 'junController',
+        templateUrl: 'Fragments/view1.html'
+    })
+     .when('/view2', {
+        controller: 'junController',
+        templateUrl: 'Fragments/view2.html'
+    })
+    .otherwise({ redirectTo: '/' });
+
+});
+
+var tigControl = {};
+
+
+
+tigControl.junController = function ($scope) {
+
+	$scope.testArray=[
+	{Name: 'Jun Alfie', Age: '26'}, 
+	{Name: 'Simud', Age: '25'}, 
+	{Name: 'Marjun', Age: '18'}, 
+	{Name: 'Jun', Age: '32'}
+	];
+
+	   $scope.addTestArray = function (){
+		$scope.testArray.push(
+			{ Name: $scope.newTestArray.Name, Age: $scope.newTestArray.Age }
+		);
+	}
+  
+}
+
+	
+
+junApp.controller(tigControl);
